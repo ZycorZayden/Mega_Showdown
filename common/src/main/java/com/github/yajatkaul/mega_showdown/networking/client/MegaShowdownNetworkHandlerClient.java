19254c22e5ab30598873d9afb727d907eae47fae
Provismet <17149901+Provismet@users.noreply.github.com>
2026-03-04 22:43:00 +0000

    Add GUI for battle data
    (cherry picked from commit 72861758104eb67f56ccfc5b9abe196114b77cfc)

--- common/src/main/java/com/github/yajatkaul/mega_showdown/networking/client/MegaShowdownNetworkHandlerClient.java
+++ common/src/main/java/com/github/yajatkaul/mega_showdown/networking/client/MegaShowdownNetworkHandlerClient.java
@@ -1,5 +1,7 @@
 package com.github.yajatkaul.mega_showdown.networking.client;
 
+import com.github.yajatkaul.mega_showdown.api.lilycobble.networking.battle.BattleStatePacketS2C;
+import com.github.yajatkaul.mega_showdown.client.battle.hud.BattleHud;
 import com.github.yajatkaul.mega_showdown.networking.client.handler.ConfigSyncHandler;
 import com.github.yajatkaul.mega_showdown.networking.client.handler.ElementSyncHandler;
 import com.github.yajatkaul.mega_showdown.networking.client.handler.InteractionWheelHandler;
@@ -12,7 +14,7 @@ public class MegaShowdownNetworkHandlerClient {
     public static void register() {
         NetworkManager.registerReceiver(NetworkManager.Side.S2C, InteractionWheelPacket.TYPE, InteractionWheelPacket.STREAM_CODEC, InteractionWheelHandler::handle);
         NetworkManager.registerReceiver(NetworkManager.Side.S2C, ConfigSyncPacket.TYPE, ConfigSyncPacket.STREAM_CODEC, ConfigSyncHandler::handle);
-
+        NetworkManager.registerReceiver(NetworkManager.Side.S2C, BattleStatePacketS2C.ID, BattleStatePacketS2C.PACKET_CODEC, BattleHud::receivePacket);
         NetworkManager.registerReceiver(NetworkManager.Side.S2C, ElementsSyncPacket.TYPE, ElementsSyncPacket.STREAM_CODEC, ElementSyncHandler::handle);
     }
 }
